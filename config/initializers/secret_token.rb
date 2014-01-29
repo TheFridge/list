8b1e204b23e8af3251618ddc79d9213f33067eff
Billy <navyosu@gmail.com>
2014-01-29 15:14:09 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FridgeList::Application.config.secret_key_base = 'b92143c1e8e5c18264bb8e1cf46d7cc2fce2dc9c5cb07371cf68d2e67c524df64b6714c898283aa250c4779a773fa935959cb7e3679f4300c56ea19f00d1f68b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FridgeList::Application.config.session_store :cookie_store, key: '_fridge_list_session'
