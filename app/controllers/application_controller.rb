fb6b721acdf46c5a13b3edcd31e92303ec628f94
Rolen Le <rolentle@gmail.com>
2014-02-12 11:03:24 -0700

    fixes for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  before_action :authenticate
   TOKEN = ENV['SUPER_SECRET_TOKEN']
+  before_action :authenticate
 
   def authenticate
     authenticate_or_request_with_http_token do |token, options|
